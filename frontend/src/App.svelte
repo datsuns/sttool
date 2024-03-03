d29effb127f6ee6c7a0293cc9816607f914679eb
datsuns <the.skeleton7@gmail.com>
2024-03-04 08:47:00 +0900

    add simple grid

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,11 +1,14 @@
 <script>
   import Button, { Label } from '@smui/button';
+  import LayoutGrid, { Cell } from '@smui/layout-grid';
+  import Card, { Content, PrimaryAction, Media, MediaContent, Actions, ActionButtons, ActionIcons, } from '@smui/card';
   import { OpenURL } from '../wailsjs/go/main/App.js';
   import { DebugRaidTest  } from '../wailsjs/go/main/App.js';
-  import { LogPrint } from '../wailsjs/runtime/runtime'
+  import { LogPrint, EventsOn } from '../wailsjs/runtime/runtime'
 
   let clicked = 0;
   let dbg_RaidUser = "";
+  let Clips = [];
 
   function openLink(url) {
     OpenURL(url).then(result => LogPrint("Opened2"));
@@ -14,6 +17,15 @@
   const callDebugRaidTest = async () => {
     await DebugRaidTest(dbg_RaidUser);
   };
+
+  EventsOn("OnRaid", (msg, username, items) => {
+    let entry = { name: username, body: items}
+    LogPrint(`raid from ${username}`)
+    items.forEach(c => {
+        LogPrint(`user clip [${c.Title}]`);
+    });
+    Clips = [...Clips, entry];
+  });
 
 </script>
 
@@ -29,6 +41,23 @@
   <input bind:value={dbg_RaidUser}  class="input" placeholder="debug raid user" />
   <button class="btn" on:click={() => openLink('https://www.google.com/')}>Greet</button>
   <button on:click={callDebugRaidTest}>raid test</button>
+  {#each Clips as clip}
+    <h1>{clip.name} さんのクリップ</h1>
+    <LayoutGrid>
+      {#each clip.body as c}
+        <Card>
+          <PrimaryAction on:click={() => openLink(c.Url)}>
+            <Media aspectRatio="16x9">
+              <img src={c.Thumbnail} alt={c.Title}/>
+            </Media>
+            <Content class="mdc-typography--body2" style="color: blue;">
+              {c.Title}
+            </Content>
+          </PrimaryAction>
+        </Card>
+      {/each}
+    </LayoutGrid>
+  {/each}
 </main>
 
 <style>
