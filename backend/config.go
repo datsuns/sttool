aa6f5aa1a5708c15f449defb158104ca1e8626ef
datsuns <the.skeleton7@gmail.com>
2024-04-14 12:56:10 +0900

    improve: add Auth entry in Config

--- backend/config.go
+++ backend/config.go
@@ -11,6 +11,7 @@ type ConfigBody struct {
 	TargetUser                   string   `yaml:"SUBSCRIBE_USER"`
 	AuthCode                     string   `yaml:"AUTH_CODE"`
 	ClientId                     string   `yaml:"CLIENT_ID"`
+	ClientSecret                 string   `yaml:"CLIENT_SECRET"`
 	ChatTargets                  []string `yaml:"CHART_TARGETS"`
 	NotifySoundFile              string   `yaml:"NOTIFY_SOUND"`
 	DebugMode                    bool     `yaml:"DEBUG"`
@@ -25,30 +26,61 @@ type ConfigBody struct {
 	OverlayEnabled               bool     `yaml:"OVERLAY_ENABLE"`
 }
 
+type AuthEntry struct {
+	AuthCode     string `yaml:"AUTH_CODE"`
+	RefreshToken string `yaml:"REFRESH_TOKEN"`
+}
+
 type Config struct {
 	Body         ConfigBody
+	Auth         AuthEntry
 	TargetUserId string
 	StatsLogPath string
 	RaidLogPath  string
 }
 
-func loadConfigFrom(raw []byte) (*Config, error) {
-	ret := &Config{
-		Body: ConfigBody{
-			NotifySoundFile:              NotifySoundDefault,
-			LogDest:                      ".",
-			DelayMinutesFromRaidToStop:   3,
-			NewClipWatchIntervalSecond:   128,
-			LocalServerPortNumber:        8930,
-			ClipPlayIntervalMarginSecond: 8,
-			OverlayEnabled:               false,
-		},
+var (
+	DefaultConfig = ConfigBody{
+		TargetUser:                   "",
+		AuthCode:                     "",
+		ClientId:                     "",
+		ClientSecret:                 "",
+		ChatTargets:                  []string{},
+		NotifySoundFile:              NotifySoundDefault,
+		DebugMode:                    false,
+		LocalTest:                    false,
+		LogDest:                      ".",
+		ObsUrl:                       "",
+		ObsPass:                      "",
+		DelayMinutesFromRaidToStop:   3,
+		NewClipWatchIntervalSecond:   128,
+		LocalServerPortNumber:        8930,
+		ClipPlayIntervalMarginSecond: 8,
+		OverlayEnabled:               false,
 	}
+)
+
+func loadConfigFrom(raw []byte) (*Config, error) {
+	ret := &Config{}
+	ret.Init()
 	if e := yaml.Unmarshal(raw, &ret.Body); e != nil {
 		return nil, e
 	}
-	ret.StatsLogPath = StatsLogPath
-	ret.RaidLogPath = RaidLogPath
+	return ret, nil
+}
+
+func setDefaultConfig(path string) (*Config, error) {
+	var err error
+	raw, err := yaml.Marshal(DefaultConfig)
+	if err != nil {
+		return nil, err
+	}
+	err = os.WriteFile(path, raw, 0644)
+	if err != nil {
+		return nil, err
+	}
+	ret := &Config{}
+	ret.Init()
 	return ret, nil
 }
 
@@ -56,13 +88,20 @@ func LoadConfig() (*Config, error) {
 	var e error
 	f, e := os.Open(ConfigFilePath)
 	if e != nil {
-		return nil, e
+		return setDefaultConfig(ConfigFilePath)
 	}
 	b, e := io.ReadAll(f)
 	if e != nil {
 		return nil, e
 	}
 	return loadConfigFrom(b)
+}
+
+func (c *Config) Init() {
+	c.Body = DefaultConfig
+	c.Auth = AuthEntry{AuthCode: "", RefreshToken: ""}
+	c.StatsLogPath = StatsLogPath
+	c.RaidLogPath = RaidLogPath
 }
 
 func (c *Config) Save() error {
@@ -88,6 +127,10 @@ func (c *Config) AuthCode() string {
 
 func (c *Config) ClientId() string {
 	return c.Body.ClientId
+}
+
+func (c *Config) ClientSecret() string {
+	return c.Body.ClientSecret
 }
 
 func (c *Config) IsDebug() bool {
